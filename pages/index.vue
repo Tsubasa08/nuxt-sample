cf7343056ba59e3c775c7aa6f9cb1f769764407f
Tsubasa Masaki <tsubasa.masaki@medpeer.co.jp>
2023-07-15 11:35:30 +0900

    save

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Topページ</h1>
+  <button @click="enableCustomLayout">Update layout</button>
+</template>
+
+<script setup>
+const route = useRoute();
+const enableCustomLayout = () => {
+  route.meta.layout = "custom";
+};
+definePageMeta({
+  layout: false,
+});
+</script>
