aa547dea9c492ae133c9be0f88402c6a0408b6df
Tsubasa Masaki <tsubasa.masaki@medpeer.co.jp>
2023-08-08 09:48:57 +0900

    plugins

--- pages/index.vue
+++ pages/index.vue
@@ -3,19 +3,11 @@
     <NuxtLayout name="default">
       <template #header>ヘッダー</template>
       <h1>Main Page</h1>
-      <img src="~/assets/icon.ico" alt="" />
-      <div>Count: {{ count }}</div>
-      <div>
-        <button @click="() => inc()">increase</button>
-        <button @click="() => dec()">decrease</button>
-      </div>
+      <h2>{{ $hello("World") }}</h2>
     </NuxtLayout>
   </div>
 </template>
 
 <script setup>
-const { count, inc, dec } = useCounter(100);
-definePageMeta({
-  layout: false,
-});
+const { $hello } = useNuxtApp();
 </script>
